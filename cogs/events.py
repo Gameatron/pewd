266f403ac492baafec68af168c4ec62efe960904
Gameatron <takodaroberts@gmail.com>
2020-04-14 14:19:35 -0500

    leave

--- cogs/events.py
+++ cogs/events.py
@@ -11,5 +11,10 @@ class Events(commands.Cog):
         channel = get(ctx.guild.channels, name="welcome")
         await channel.send(f"Welcome dumdum ({ctx.mention}), this is a new server and it is still in construction. If you stay you will be part if a great disfunctional disc family, and if you leave ur a fag. Now introduce yourself")
     
+    @commands.Cog.listener()
+    async def on_member_remove(self, ctx):
+        channel = get(ctx.guild.channels, name="welcome")
+        await channel.send(f"**{ctx.name}** is a fag for leaving-!")
+
 def setup(bot):
     bot.add_cog(Events(bot))
